2d29e7d6d90305ad119776ac2cc55c9b12b8508c
Sai <gajulapalli.aditya@gmail.com>
2017-05-22 21:53:53 -0400

    to test maven

---
+++ MyMaven_Artifact_New/src/main/java/MyMaven_GrpId/App.java
@@ -0,0 +1,13 @@
+package MyMaven_GrpId;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
